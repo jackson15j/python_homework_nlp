a155b1a9ca9e84df242dab30124afa8b6db7662a
Craig Astill <craig.astill@gmail.com>
2021-12-26 08:00:30 +0000

    Extra verbose in emacs `pytest` compile command.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "cd $(git rev-parse --show-toplevel) && poetry run black .;poetry run pycodestyle .;poetry run flake8 .;poetry run mypy .;poetry run pytest -v")))
- (python-mode . ((compile-command . "cd $(git rev-parse --show-toplevel) && poetry run black .;poetry run pycodestyle .;poetry run flake8 .;poetry run mypy .;poetry run pytest -v"))))
+((nil . ((compile-command . "cd $(git rev-parse --show-toplevel) && poetry run black .;poetry run pycodestyle .;poetry run flake8 .;poetry run mypy .;poetry run pytest -vv")))
+ (python-mode . ((compile-command . "cd $(git rev-parse --show-toplevel) && poetry run black .;poetry run pycodestyle .;poetry run flake8 .;poetry run mypy .;poetry run pytest -vv"))))
