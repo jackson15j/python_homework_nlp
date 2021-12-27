d7189e637512db59697baffcf084bfc4ab5256fa
Craig Astill <craig.astill@gmail.com>
2021-12-27 15:49:33 +0000

    Simplify Renderers import path.

---
+++ python_homework_nlp/renderers/__init__.py
@@ -0,0 +1,9 @@
+from python_homework_nlp.renderers.base_renderer import BaseRenderer
+from python_homework_nlp.renderers.csv_renderer import CsvRenderer
+from python_homework_nlp.renderers.json_renderer import JsonRenderer
+
+__all__ = [
+    "BaseRenderer",
+    "CsvRenderer",
+    "JsonRenderer",
+]
