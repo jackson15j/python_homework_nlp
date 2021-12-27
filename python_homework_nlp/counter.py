5496c10ae1f0d9de7aac585fbfdafb408e7a59fb
Craig Astill <craig.astill@gmail.com>
2021-12-27 14:35:44 +0000

    Convert `counter.py` to a class to contain logic + re-use `Context`.

--- python_homework_nlp/counter.py
+++ python_homework_nlp/counter.py
@@ -1,74 +1,100 @@
 import collections
+from python_homework_nlp.common import Content
 
 
-def _get_word_counts(tokens: list) -> collections.Counter:
-    return collections.Counter(tokens)
-
-
-def _sum_collection_counters(counters: list) -> collections.Counter:
-    total_counter: collections.Counter = collections.Counter()
-    for x in counters:
-        total_counter += x
-    return total_counter
-
-
-def _get_matches(word: str, orig: dict) -> dict:
-    """Returns all files + sentences that match a given word.
-
-    :param str word: word to find matches for.
-    :param dict orig: Original nested dictionary of files and original sentences
-    + filtered tokens.
-    {<filename>: {"original_sentences": [<str>,], "filtered_tokens": [[<str>,],]}}
-    :returns: dict of: {"matches": <list>, "files": <list>}
+class Counter:
+    """Class for counting/discovery of most used words + files & original
+    sentences.
     """
-    # TOOD: test
-    _matched_files = []
-    _matched_sentences = []
-    for _file in orig:
-        _matches = _get_matches_by_word(word, orig[_file]["original_sentences"])
-        if _matches:
-            _matched_files.append(_file)
-            _matched_sentences.extend(_matches)
 
-    return {"matches": _matched_sentences, "files": _matched_files}
+    content_objs: list[Content]
 
+    def __init__(self, content_objs: list[Content]) -> None:
+        self.content_objs = content_objs
 
-def _get_matches_by_word(word: str, original_sentences: list) -> list:
-    """Returns all sentences that match a given word.
+    @staticmethod
+    def _get_word_counts(tokens: list[str]) -> collections.Counter:
+        """Gets a `collections.Counter` instance with all words and their
+        respective totals for the provided list of tokens (words)
+        """
+        # TODO: Move this logic into `common.Content`, ie. write to:
+        # `Content._collections_counter` on writes to
+        # `Content.filtered_tokens`. Only expose getter for
+        # `Content.collections_counter`.j
+        print(tokens)
+        return collections.Counter(tokens)
 
-    :param str word: word to find matches for.
-    :param list orig_sentences: List of sentences to find matches in.
-    :returns: list of matched sentences.
-    """
-    # TOOD: test
-    return [x for x in original_sentences if word in x.lower()]
+    @staticmethod
+    def _sum_collection_counters(
+        counters: list[collections.Counter],
+    ) -> collections.Counter:
+        """Collect together each sentence-level `collections.Counter` instance,
+        so that we can return a singular (file-level) `collections.Counter`
+        instance.
+        """
+        total_counter: collections.Counter = collections.Counter()
+        for x in counters:
+            total_counter += x
+        return total_counter
 
+    def _get_matches(self, word: str) -> dict:
+        """Returns all files + sentences that match a given word.
 
-def counter(orig: dict, most_common: int = None) -> dict:
-    """Counts number of words duplicate words in a filtered tokens list +
-    generates the return structure.
+        :param str word: word to find matches for.
+        :returns: dict of: {"matches": <list>, "files": <list>}
+        """
+        # TOOD: test
+        _matched_files = []
+        _matched_sentences = []
+        for content in self.content_objs:
+            _matches = Counter._get_matches_by_word(
+                word, content.original_sentences
+            )
+            if _matches:
+                _matched_files.append(content.file_name)
+                _matched_sentences.extend(_matches)
 
-    :param dict orig: Original nested dictionary of files and original sentences
-    + filtered tokens.
-    {<filename>: {"original_sentences": [<str>,], "filtered_tokens": [[<str>,],]}}
-    :param int most_common: Returns X most common words. Returns all if None.
-    :returns: dict of:
-    {<word>: {"count": <int>, "matches": [<str>,], "files": [<str>,]}}
-    Where:
-    * `count` = number of matches.
-    * `matches` = list of matched sentences.
-    * `files` = list of filenames with matched sentences.
-    """
-    # TODO: separate counter logic from result dict generation ??
-    # TODO: reduce the number of loops to generate the `ret_dict`!!
-    ret_dict = {}
-    _counters = [
-        _get_word_counts(x) for k, v in orig.items() for x in v["filtered_tokens"]
-    ]
-    _total_counter = _sum_collection_counters(_counters)
-    ret_dict = {
-        k: {"count": v} for k, v in _total_counter.most_common(most_common)
-    }
-    for _word in ret_dict.keys():
-        ret_dict[_word].update(_get_matches(_word, orig))
-    return ret_dict
+        # TODO: Update `Content` to have these fields, or move out to `Renderer`??
+        return {"matches": _matched_sentences, "files": _matched_files}
+
+    @staticmethod
+    def _get_matches_by_word(word: str, original_sentences: list[str]) -> list:
+        """Returns all sentences that match a given word.
+
+        :param str word: word to find matches for.
+        :param list orig_sentences: List of sentences to find matches in.
+        :returns: list of matched sentences.
+        """
+        # TOOD: test
+        return [x for x in original_sentences if word in x.lower()]
+
+    def counter(self, most_common: int = None) -> dict:
+        """Counts number of words duplicate words in a filtered tokens list +
+        generates the return structure.
+
+        :param int most_common: Returns X most common words. Returns all if None.
+        :returns: dict of:
+        {<word>: {"count": <int>, "matches": [<str>,], "files": [<str>,]}}
+        Where:
+        * `count` = number of matches.
+        * `matches` = list of matched sentences.
+        * `files` = list of filenames with matched sentences.
+        """
+        # TODO: separate counter logic from result dict generation ??
+        # TODO: reduce the number of loops to generate the `ret_dict`!!
+        ret_dict = {}
+
+        # TODO: Store `collection.Counters` for sentences + totals, both by
+        # files ?? Or Just Total by file ??
+        _counters = [
+            Counter._get_word_counts(y)
+            for x in self.content_objs
+            for y in x.filtered_tokens
+        ]
+        _total_counter = Counter._sum_collection_counters(_counters)
+        ret_dict = {
+            k: {"count": v} for k, v in _total_counter.most_common(most_common)
+        }
+        for _word in ret_dict.keys():
+            ret_dict[_word].update(self._get_matches(_word))
+        return ret_dict
